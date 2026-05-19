8ffc643925224dc870ce18942f881595c6425066
Xamide89 <0702akim@gmail.com>
2026-05-20 00:42:47 +0200

    Implement metrics collection and sending functionality

---
+++ agent/agent.py
@@ -0,0 +1,38 @@
+import psutil
+import socket
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+
+def collect_metrics():
+    return {
+        "host": socket.gethostname(),
+        "cpu": psutil.cpu_percent(interval=1),
+        "ram": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage("C:\\").percent
+    }
+
+def send_metrics():
+    data = collect_metrics()
+
+    try:
+        response = requests.post(
+            API_URL,
+            json=data,
+            timeout=10
+        )
+
+        print(
+            f"Sent metrics: "
+            f"{response.status_code}"
+        )
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    send_metrics()
